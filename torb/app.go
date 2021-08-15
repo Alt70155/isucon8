e6a76fe268a752e02d8b65333ef476f50d8fa059
Alt70155 <chika1086@gmail.com>
2021-08-16 01:09:45 +0900

    log fix

--- torb/app.go
+++ torb/app.go
@@ -564,27 +564,27 @@ func main() {
 			fmt.Println("[My Log] my event: ", event.ID, event.Title)
 		}
 		// 前の実装
-		// var recentEvents []*Event
-		// for rows.Next() {
-		// 	var eventID int64
-		// 	if err := rows.Scan(&eventID); err != nil {
-		// 		return err
-		// 	}
-		// 	event, err := getEvent(eventID, -1)
-		// 	if err != nil {
-		// 		return err
-		// 	}
-		// 	for k := range event.Sheets {
-		// 		event.Sheets[k].Detail = nil
-		// 	}
-		// 	recentEvents = append(recentEvents, event)
-		// }
-		// if recentEvents == nil {
-		// 	recentEvents = make([]*Event, 0)
-		// }
-		// for _, event := range recentEvents {
-		// 	fmt.Println("[My Log] default event: ", event.ID, event.Title)
-		// }
+		var recentEvents []*Event
+		for rows.Next() {
+			var eventID int64
+			if err := rows.Scan(&eventID); err != nil {
+				return err
+			}
+			event, err := getEvent(eventID, -1)
+			if err != nil {
+				return err
+			}
+			for k := range event.Sheets {
+				event.Sheets[k].Detail = nil
+			}
+			recentEvents = append(recentEvents, event)
+		}
+		if recentEvents == nil {
+			recentEvents = make([]*Event, 0)
+		}
+		for _, event := range recentEvents {
+			fmt.Println("[My Log] default event: ", event.ID, event.Title)
+		}
 
 		return c.JSON(200, echo.Map{
 			"id":                  user.ID,
